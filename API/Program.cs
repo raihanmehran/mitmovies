3e58270371d2d8361a31d06d921f9b92500f40e8
Mehran <raihan.mehran@hotmail.com>
2023-04-25 22:45:47 +0200

    API: Program.cs Configured for Db

--- API/Program.cs
+++ API/Program.cs
@@ -1,4 +1,7 @@
 using API.Extensions;
+using API.Middlewares;
+using Infrastructure.v1.Contexts;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -7,14 +10,27 @@ builder.Services.AddApplicationServices(builder.Configuration);
 
 var app = builder.Build();
 
+app.UseMiddleware<ExceptionMiddleware>();
+
 app.UseCors(builder => builder
     .AllowAnyHeader()
     .AllowAnyMethod()
     .AllowCredentials()
     .WithOrigins("https://localhost:4200", "http://localhost:4200"));
 
-app.UseHttpsRedirection();
-
 app.MapControllers();
+
+using var scope = app.Services.CreateScope();
+var services = scope.ServiceProvider;
+try
+{
+    var context = services.GetRequiredService<DataContext>();
+    await context.Database.MigrateAsync();
+}
+catch (Exception ex)
+{
+    var logger = services.GetService<ILogger<Program>>();
+    logger.LogError(ex, "An error occured during migrations");
+}
 
 app.Run();
