8e0b524fbedc55767b2525abb5fb1e71f1c8f060
Mehran <raihan.mehran@hotmail.com>
2023-04-25 22:36:50 +0200

    API: Db Connection Configured

--- API/Extensions/ApplicationServiceExtensions.cs
+++ API/Extensions/ApplicationServiceExtensions.cs
@@ -1,4 +1,6 @@
+using Infrastructure.v1.Contexts;
 using MediatR;
+using Microsoft.EntityFrameworkCore;
 
 namespace API.Extensions
 {
@@ -8,6 +10,10 @@ namespace API.Extensions
             this IServiceCollection services, IConfiguration config
         )
         {
+            services.AddDbContext<DataContext>(opt =>
+            {
+                opt.UseSqlite(config.GetConnectionString("DefaultConnection"));
+            });
             services.AddCors();
             services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
             services.AddMediatR(AppDomain.CurrentDomain.GetAssemblies());
