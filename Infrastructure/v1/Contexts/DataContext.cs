692830f7bf6c6b412b72d524529bee837f1231c4
Mehran <raihan.mehran@hotmail.com>
2023-04-25 20:56:04 +0200

    INFRA: Tables added to DataContext.cs

--- Infrastructure/v1/Contexts/DataContext.cs
+++ Infrastructure/v1/Contexts/DataContext.cs
@@ -1,5 +1,5 @@
+using Domain.v1.Entities;
 using Microsoft.EntityFrameworkCore;
-// using Domain.v1.Entities;
 
 namespace Infrastructure.v1.Contexts
 {
@@ -8,9 +8,21 @@ namespace Infrastructure.v1.Contexts
         public DataContext(DbContextOptions options)
             : base(options: options)
         {
-
         }
 
-        // public DbSet<Cast> Casts { get; set; }
+        public DbSet<Cast> Casts { get; set; }
+        public DbSet<Credit> Credits { get; set; }
+        public DbSet<Crew> Crews { get; set; }
+        public DbSet<Genre> Genres { get; set; }
+        public DbSet<Keyword> Keywords { get; set; }
+        public DbSet<Movie> Movies { get; set; }
+        public DbSet<MovieCollection> MovieCollections { get; set; }
+        public DbSet<MovieKeywords> MovieKeywords { get; set; }
+        public DbSet<MovieLinks> MovieLinks { get; set; }
+        public DbSet<ProductionCompany> ProductionCompanies { get; set; }
+        public DbSet<ProductionCountry> ProductionCountries { get; set; }
+        public DbSet<UserRating> Ratings { get; set; }
+        public DbSet<SpokenLanguage> SpokenLanguages { get; set; }
+
     }
 }
