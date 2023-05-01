05e2e883590737757e649240558d4c1efb8914fd
Mehran <raihan.mehran@hotmail.com>
2023-05-01 14:01:13 +0200

    GetMovieById_Returns_NotFoundResult_WhenMovieDoesNotExist -> PASS

--- MitMovies.Tests.Unit/Controllers/v1/MoviesControllerTests/MoviesControllerTests.Retrieve.GetMovieById.cs
+++ MitMovies.Tests.Unit/Controllers/v1/MoviesControllerTests/MoviesControllerTests.Retrieve.GetMovieById.cs
@@ -24,10 +24,29 @@ namespace MitMovies.Tests.Unit.Controllers.v1.MoviesControllerTests
             var response = await _controller.GetMovieById(movieId: movieId);
 
             // Assert
+            Assert.NotNull(response);
             Assert.IsType<ActionResult<ResponseMessage>>(response);
             var okResult = Assert.IsType<OkObjectResult>(response.Result);
             var movie = Assert.IsType<Movie>(okResult.Value);
             Assert.Equal(expectedMovieData.Id, movie.Id);
+        }
+
+        [Fact]
+        public async void GetMovieById_Returns_NotFoundResult_WhenMovieDoesNotExist()
+        {
+            // Arrange
+            int movieId = 999999999;
+            var expectedMovieData = new ResponseMessage { Data = null };
+            _mediatorMock.Setup(x => x.Send(It.Is<GetMovieByIdQuery>(q =>
+                q.MovieId == movieId), default))
+                .ReturnsAsync(expectedMovieData);
+
+            // Act
+            var response = await _controller.GetMovieById(movieId: movieId);
+
+            // Assert
+            Assert.IsType<NotFoundObjectResult>(response.Result);
+            Assert.Null(response.Value);
         }
     }
 }
