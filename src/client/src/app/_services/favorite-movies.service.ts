40a7895ee16fde819eb689015f5be8d5b7674cee
Raihan Mehran <raihan.mehran@hotmail.com>
2023-06-11 23:17:44 +0200

    CLIENT: FavoriteMovies Service Added

--- src/client/src/app/_services/favorite-movies.service.ts
+++ src/client/src/app/_services/favorite-movies.service.ts
@@ -8,6 +8,7 @@ import { environment } from 'src/environments/environment';
 })
 export class FavoriteMoviesService {
   baseUrl = environment.apiUrl;
+  favoriteMovies: any[] = [];
 
   constructor(private http: HttpClient) {}
 
@@ -15,5 +16,25 @@ export class FavoriteMoviesService {
     this.http
       .post(this.baseUrl + 'favouritemovies/add/' + id, id)
       .pipe(map((response) => response));
+  }
+
+  removeFromFavoriteMovies(id: number) {
+    this.http
+      .post(this.baseUrl + 'favouritemovies/remove/' + id, id)
+      .pipe(map((response) => response));
+  }
+
+  getFavoriteMovies() {
+    this.http
+      .get<any[]>(this.baseUrl + 'favouritemovies')
+      .pipe()
+      .subscribe({
+        next: (movies) => {
+          console.log(movies);
+
+          this.favoriteMovies = movies;
+        },
+        error: (error) => console.log(error.error),
+      });
   }
 }
