325b496354d3b4e903ce311b99fa7681f1bb0cc7
Raihan Mehran <raihan.mehran@hotmail.com>
2023-07-02 00:45:32 +0200

    OPTIMIZATION: UpcomingMoviesService Optimized

--- src/client/src/app/_services/upcoming-movies.service.ts
+++ src/client/src/app/_services/upcoming-movies.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable, of, tap } from 'rxjs';
 import { environment } from 'src/environments/environment';
 
 @Injectable({
@@ -7,10 +8,19 @@ import { environment } from 'src/environments/environment';
 })
 export class UpcomingMoviesService {
   private baseUrl = environment.apiUrl;
+  private cache: { [key: string]: any } = {};
 
   constructor(private http: HttpClient) {}
 
-  getUpcomingMovies(page: number) {
-    return this.http.get<any[]>(this.baseUrl + 'movies/upcoming?page=' + page);
+  getUpcomingMovies(page: number): Observable<any> {
+    const cacheKey = `upcoming${page}`;
+
+    if (this.cache[cacheKey]) {
+      return of(this.cache[cacheKey]);
+    }
+
+    return this.http
+      .get<any[]>(this.baseUrl + 'movies/upcoming?page=' + page)
+      .pipe(tap((response) => (this.cache[cacheKey] = response)));
   }
 }
