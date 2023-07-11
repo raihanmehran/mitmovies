6e61cc820b14d8e6a08379622c2f82c7bbbb06cd
Raihan Mehran <raihan.mehran@hotmail.com>
2023-07-12 01:03:07 +0200

    GRAPHQL: GetWatchLaterMovies Added

--- src/backend/API/Resolvers/Queries/UserDetailQueires.cs
+++ src/backend/API/Resolvers/Queries/UserDetailQueires.cs
@@ -98,6 +98,55 @@ namespace API.Resolvers.Queries
             catch (Exception) { throw; }
         }
 
+        [Authorize(Policy = "RequireAuthenticated")]
+        public async ValueTask<List<Movie>> GetWatchLaterMovies(
+            [Service] DataContext context,
+            [Service] IHttpContextAccessor httpContextAccessor
+        )
+        {
+            try
+            {
+                var movies = new List<Movie>();
+                var userId = httpContextAccessor.HttpContext.User.GetUserId();
+
+                if (userId <= 0) return movies;
+
+                var user = await context.Users
+                    .Where(x => x.Id == userId)
+                    .Select(x => new
+                    {
+                        userId = x.Id,
+                        watchLaterMovies = x.WatchLaters.Where(x => x.MovieId != 0).ToList()
+                    })
+                    .AsNoTracking()
+                    .SingleOrDefaultAsync();
+
+                if (user.watchLaterMovies.Count == 0) return movies;
+
+                foreach (var movie in user.watchLaterMovies)
+                {
+                    var result = await _moviesRepository
+                        .GetMovieByIdAsync(movieId: movie.MovieId);
+
+                    if (result.Data is not null) movies.Add(result.Data as Movie);
+                }
+
+                // Using Multi threadding
+                // var tasks = user.watchLaterMovies.Select(async movie =>
+                // {
+                //     var result = await _moviesRepository.GetMovieByIdAsync(movieId: movie.MovieId);
+                //     return result.Data as Movie;
+                // });
+
+                // var movieResults = await Task.WhenAll(tasks);
+                // movies.AddRange(movieResults.Where(movie => movie is not null));
+
+
+                return movies;
+            }
+            catch (Exception) { throw; }
+        }
+
         public string Hello()
         {
             return "Hello";
