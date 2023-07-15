3e0be012864276d00709cfbc220e54047bcba5af
Raihan Mehran <raihan.mehran@hotmail.com>
2023-07-16 00:00:03 +0200

    Added

---
+++ src/client/src/app/components/member/member-people-interactions/member-people-interactions.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-member-people-interactions',
+  templateUrl: './member-people-interactions.component.html',
+  styleUrls: ['./member-people-interactions.component.css']
+})
+export class MemberPeopleInteractionsComponent {
+
+}
